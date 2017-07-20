6dc7b3b672bef28b2ae1135fbd9a7936fe15bc56
Eddie Antonio Santos <easantos@ualberta.ca>
2017-07-20 10:57:32 -0600

    Tidy up distance.py

--- verify-pairs/distance.py
+++ verify-pairs/distance.py
@@ -1,23 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+# Copyright 2017 Eddie Antonio Santos <easantos@ualberta.ca>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Determines the Levenshtein distance between two source files.
+"""
+
 import sqlite3
 from typing import Iterable, Iterator, NewType, Optional, Tuple, cast
 
+from javalang.tokenizer import LexerError  # type: ignore
+from Levenshtein import distance, editops  # type: ignore
+
+# Use tqdm only if it's installed.
 try:
     from tqdm import tqdm  # type: ignore
 except ImportError:
     def tqdm(it, *_args, **_kwargs):
         yield from it
 
-from Levenshtein import distance, editops  # type: ignore
-from javalang.tokenizer import LexerError  # type: ignore
-
-from vocabulary import vocabulary, Vind
-from lexical_analysis import Lexeme
 from java import java, java2sensibility
+from lexical_analysis import Lexeme
+from vocabulary import vocabulary, Vind
 
 
+# Different types of IDs that are both just ints, but it's very important
+# that the IDs don't get confused for one-another.
 SFID = NewType('SFID', int)
 MEID = NewType('MEID', int)
 Revision = Tuple[SFID, MEID]
@@ -39,21 +60,66 @@ PUA_B_START = 0x100000
 
 
 class Mistake:
-    def __init__(self, sfid: SFID, meid: MEID, before: bytes, after: bytes) -> None:
+    """
+    Represents a mistkae in the database.
+    """
+    def __init__(self, sfid: SFID, meid: MEID,
+                 before: bytes, after: bytes) -> None:
         self.sfid = sfid
         self.meid = meid
         self.before = before
         self.after = after
 
 
+class EditType:
+    """
+    Symbolic constants for Insertion, Deletion, and Substitution.
+    """
+    def __repr__(self) -> str:
+        return type(self).__name__
+
+
+Insertion = type('Insertion', (EditType,), {})()
+Deletion = type('Deletion', (EditType,), {})()
+Substitution = type('Substitution', (EditType,), {})()
+
+
+def to_edit_type(name: str) -> EditType:
+    """
+    Convert a string returned from Levenshtein.editops into a constant.
+    """
+    return {
+        'replace': Substitution,
+        'insert': Insertion,
+        'delete': Deletion
+    }[name]
+
+
+# TODO: use Edit classes from sensibility?
+class Edit:
+    """
+    Represents an edit.
+    """
+    __slots__ = 'type', 'position', 'new_token'
+
+    def __init__(self, type_name: EditType, position: int,
+                 new_token: Optional[Vind]) -> None:
+        self.type = type_name
+        self.position = position
+        self.new_token = new_token
+
+
 # TODO: factor out with class from verify-pair?
 class Mistakes(Iterable[Mistake]):
+    """
+    Access to the mistake database.
+    """
     def __init__(self, conn: sqlite3.Connection) -> None:
         self.conn = conn
         self.conn.executescript(SCHEMA)
 
     def __iter__(self) -> Iterator[Mistake]:
-        query = '''SELECT source_file_id, before_id, before, after FROM mistake'''
+        query = 'SELECT source_file_id, before_id, before, after FROM mistake'
         for row in self.conn.execute(query):
             yield Mistake(*row)
 
@@ -69,7 +135,9 @@ def tokens2seq(tokens: Iterable[Lexeme],
                to_index=vocabulary.to_index,
                to_entry=java2sensibility) -> str:
     """
-    Converts a sequence of tokens into a special string
+    Converts a sequence of tokens into an encoded string, that preserves
+    distinctness between tokens. This string can be operated by the
+    Levenshtein module.
     """
     # Injective maps of each vocabulary entry to a codepoint in the private
     # use area.
@@ -82,57 +150,48 @@ def tokens2seq(tokens: Iterable[Lexeme],
 
 
 def tokenwise_distance(file_a: bytes, file_b: bytes) -> int:
+    """
+    Calculates the token-wise Levenshtein distance between two source files.
+    """
     seq_a = tokens2seq(java.tokenize(file_a))
     seq_b = tokens2seq(java.tokenize(file_b))
     # TODO: use editops as a post-processing step!
     return distance(seq_a, seq_b)
 
 
-class EditType:
-    def __repr__(self) -> str:
-        return type(self).__name__
-
-
-Insertion = type('Insertion', (EditType,), {})()
-Deletion = type('Deletion', (EditType,), {})()
-Substitution = type('Substitution', (EditType,), {})()
-
-
-def to_edit_type(name: str) -> EditType:
-    return {
-        'replace': Substitution,
-        'insert': Insertion,
-        'delete': Deletion
-    }[name]
-
-
-class Edit:
-    __slots__ = 'type', 'position', 'new_token'
-    def __init__(self, type_name: EditType, position: int, new_token: Optional[Vind]) -> None:
-        self.type = type_name
-        self.position = position
-        self.new_token = new_token
-
-
 def determine_edit(file_a: bytes, file_b: bytes) -> Edit:
+    """
+    For two source files with Levenshtein distance of one, this returns the
+    edit that converts the first file into the second file.
+    """
     src = tokens2seq(java.tokenize(file_a))
     dest = tokens2seq(java.tokenize(file_b))
     ops = editops(src, dest)
+    # This only works for files with one edit!
     assert len(ops) == 1
+
+    # Decode editop's format.
     (type_name, src_pos, dest_pos), = ops
     edit_type = to_edit_type(type_name)
     new_token = None if edit_type is Deletion else from_pua(dest[dest_pos])
+
     return Edit(edit_type, dest_pos, new_token)
 
 
 def index_of(token: str) -> Vind:
+    """
+    Given a token in the vocabulary, returns its vocabulary index.
+    """
     return vocabulary.to_index(token)
 
 
 def from_pua(char: str) -> Vind:
+    """
+    Undoes the injective mapping between vocabulary IDs and Private Use Area
+    code poiunts.
+    """
     assert ord(char) >= 0x100000
     return cast(Vind, ord(char) & 0xFFFF)
-
 
 
 def test() -> None:
