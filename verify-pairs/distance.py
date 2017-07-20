442982aa40e944055b1ad35d721f2d29bfc97fd9
Eddie Antonio Santos <easantos@ualberta.ca>
2017-07-20 10:43:05 -0600

    Get determine_edit() working with trivial cases.

--- verify-pairs/distance.py
+++ verify-pairs/distance.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 import sqlite3
-from typing import Iterable, Iterator, Tuple, NewType
+from typing import Iterable, Iterator, NewType, Optional, Tuple, cast
 
 try:
     from tqdm import tqdm  # type: ignore
@@ -10,10 +10,10 @@ except ImportError:
     def tqdm(it, *_args, **_kwargs):
         yield from it
 
-from Levenshtein import distance  # type: ignore
+from Levenshtein import distance, editops  # type: ignore
 from javalang.tokenizer import LexerError  # type: ignore
 
-from vocabulary import vocabulary
+from vocabulary import vocabulary, Vind
 from lexical_analysis import Lexeme
 from java import java, java2sensibility
 
@@ -88,6 +88,53 @@ def tokenwise_distance(file_a: bytes, file_b: bytes) -> int:
     return distance(seq_a, seq_b)
 
 
+class EditType:
+    def __repr__(self) -> str:
+        return type(self).__name__
+
+
+Insertion = type('Insertion', (EditType,), {})()
+Deletion = type('Deletion', (EditType,), {})()
+Substitution = type('Substitution', (EditType,), {})()
+
+
+def to_edit_type(name: str) -> EditType:
+    return {
+        'replace': Substitution,
+        'insert': Insertion,
+        'delete': Deletion
+    }[name]
+
+
+class Edit:
+    __slots__ = 'type', 'position', 'new_token'
+    def __init__(self, type_name: EditType, position: int, new_token: Optional[Vind]) -> None:
+        self.type = type_name
+        self.position = position
+        self.new_token = new_token
+
+
+def determine_edit(file_a: bytes, file_b: bytes) -> Edit:
+    src = tokens2seq(java.tokenize(file_a))
+    dest = tokens2seq(java.tokenize(file_b))
+    ops = editops(src, dest)
+    assert len(ops) == 1
+    (type_name, src_pos, dest_pos), = ops
+    edit_type = to_edit_type(type_name)
+    new_token = None if edit_type is Deletion else from_pua(dest[dest_pos])
+    return Edit(edit_type, dest_pos, new_token)
+
+
+def index_of(token: str) -> Vind:
+    return vocabulary.to_index(token)
+
+
+def from_pua(char: str) -> Vind:
+    assert ord(char) >= 0x100000
+    return cast(Vind, ord(char) & 0xFFFF)
+
+
+
 def test() -> None:
     assert 1 == tokenwise_distance(b'class Hello {',    b'class Hello {}')
     assert 1 == tokenwise_distance(b'class Hello {}',   b'class Hello }')
@@ -105,10 +152,27 @@ def test_extra() -> None:
     assert 1 == tokenwise_distance(b'goto label;', b'int label;')
 
 
-def test_get_source() -> None:
+def _test_get_source() -> None:
     m = Mistakes(sqlite3.connect('java-mistakes.sqlite3'))
     mistake = next(iter(m))
     assert 0 < tokenwise_distance(mistake.before, mistake.after)
+
+
+def test_get_edit() -> None:
+    ins = determine_edit(b'class Hello {',    b'class Hello {}')
+    assert ins.type is Insertion
+    assert ins.new_token == index_of('}')
+    assert ins.position == 3
+
+    delt = determine_edit(b'class Hello {{}',   b'class Hello {}')
+    assert delt.type is Deletion
+    assert delt.new_token is None
+    assert delt.position in {2, 3}  # Can be either curly brace
+
+    sub = determine_edit(b'goto label;', b'int label;')
+    assert sub.type is Substitution
+    assert sub.position == 0
+    assert sub.new_token == index_of('int')
 
 
 if __name__ == '__main__':
